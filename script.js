58b3c169837e8a0a9df76a72dba2e7b1f38244b4
Yiheng Chen <chenyihengc.y@hotmail.com>
2023-08-20 21:31:53 -0500

    2023fall

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //https://stackoverflow.com/questions/3665115/how-to-create-a-file-in-memory-for-user-to-download-but-not-through-server
 function download(filename, text) {
-    var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    element.setAttribute('download', filename);
-  
-    element.style.display = 'none';
-    document.body.appendChild(element);
-  
-    element.click();
-  
-    document.body.removeChild(element);
-  }
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
 //https://ical.marudot.com/
 let prefix = 
 `BEGIN:VCALENDAR
@@ -46,52 +46,52 @@ let currentUTC = isoStr.slice(0, isoStr.length - 4)+"Z"
 
 //https://github.com/30-seconds/30-seconds-of-code/blob/master/snippets/UUIDGeneratorBrowser.md
 const UUIDGeneratorBrowser = () =>
-  ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-    (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
-  );
+([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+  (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
+);
 
 // convert Path @ Penn time format to UTC (substring) format that we can use  
 function timeConversion(input){
-    let clean_input = input.replaceAll(",","").replaceAll(".","")
+  let clean_input = input.replaceAll(",","").replaceAll(".","")
+  
+  if(String(clean_input).includes("pm")){
+      clean_input_list = clean_input.replaceAll("pm","").split(":")
     
-    if(String(clean_input).includes("pm")){
-        clean_input_list = clean_input.replaceAll("pm","").split(":")
+      clean_input = String(Number(clean_input_list[0])+12)+clean_input_list[1]
+  }
+  else{
+      clean_input_list = clean_input.replaceAll("am","").split(":")
+      if (Number(clean_input_list[0])<10){
+        clean_input = String("0"+clean_input_list[0]+clean_input_list[1])
+      }
+      else{
+        clean_input = String(clean_input_list[0]+clean_input_list[1])
+      }
       
-        clean_input = String(Number(clean_input_list[0])+12)+clean_input_list[1]
-    }
-    else{
-        clean_input_list = clean_input.replaceAll("am","").split(":")
-        if (Number(clean_input_list[0])<10){
-          clean_input = String("0"+clean_input_list[0]+clean_input_list[1])
-        }
-        else{
-          clean_input = String(clean_input_list[0]+clean_input_list[1])
-        }
-        
-        
-    }
-    return clean_input
+      
+  }
+  return clean_input
 }
 ///////////////
 
 // initial date designation. Here we are sacrificing the monday(even tho we start tuesday, I didn't make)
 // that into a special case to reduce complexity
 function getInitDate(input){
-  if(input == "Tuesday"){
-    return "0829"
-  }
-  else if(input == "Monday"){
-    return "0928"
-  }
-  else if(input == "Friday"){
-    return "0901"
-  }
-  else if(input == "Wednesday"){
-    return "0830"
-  }
-  else if(input == "Thursday"){
-    return "0901"
-  }
+if(input == "Tuesday"){
+  return "0829"
+}
+else if(input == "Monday"){
+  return "0928"
+}
+else if(input == "Friday"){
+  return "0901"
+}
+else if(input == "Wednesday"){
+  return "0830"
+}
+else if(input == "Thursday"){
+  return "0901"
+}
 }
 //Scrape data from the Path @ Penn page
 let a = document.querySelectorAll("body > main > div.panel.panel--2x.panel--kind-calendar.panel--visible > div > div.panel__body > div.section > div > svg > g > g.section")
@@ -105,14 +105,14 @@ let event_str = ""
 
 //iterate through each "event" and set up recurrence 
 for (let i = 0; i < unique_courses.length; i++) {
-  let course_string = unique_courses[i]
-  let course_name = course_string.split(' - ')[0]
-  let couse_schedule_string = course_string.split(' - ')[1].split(' ')
+let course_string = unique_courses[i]
+let course_name = course_string.split(' - ')[0]
+let couse_schedule_string = course_string.split(' - ')[1].split(' ')
 
-  
-  if(course_string.includes("and")){
-  //Case where there is a two-day recurrence
-    event_str = 
+
+if(course_string.includes("and")){
+//Case where there is a two-day recurrence
+  event_str = 
 `BEGIN:VEVENT
 DTSTAMP:${currentUTC}
 UID:${UUIDGeneratorBrowser()}
@@ -121,10 +121,10 @@ RRULE:FREQ=WEEKLY;BYDAY=${couse_schedule_string[0].slice(0,2).toUpperCase()},${c
 DTEND;TZID=America/Havana:2023${getInitDate(couse_schedule_string[0])}T${timeConversion(couse_schedule_string[4])}00
 SUMMARY:${course_name}
 END:VEVENT\n`
-  }
-  else{
-    //Case where there is a one-day recurrence
-    event_str = 
+}
+else{
+  //Case where there is a one-day recurrence
+  event_str = 
 `BEGIN:VEVENT
 DTSTAMP:${currentUTC}
 UID:${UUIDGeneratorBrowser()}
@@ -133,14 +133,13 @@ RRULE:FREQ=WEEKLY;BYDAY=${couse_schedule_string[0].slice(0,2).toUpperCase()};UNT
 DTEND;TZID=America/Havana:2023${getInitDate(couse_schedule_string[0])}T${timeConversion(couse_schedule_string[4])}00
 SUMMARY:${course_name}
 END:VEVENT\n`
-  }
-  
-
-  cal_str = cal_str + event_str 
 }
 
 
-  // Start file download.
-  download("course_calendar.ics",
-  cal_str);
-  
+cal_str = cal_str + event_str 
+}
+
+
+// Start file download.
+download("course_calendar.ics",
+cal_str);
