65c5137df086e6b834bae11fde8a41e538da0e2f
intelc <32405016+intelc@users.noreply.github.com>
2026-01-16 10:20:07 +0900

    Update semester dates in script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 //////////////////////
 //PLEASE UPDATE THE BELOW PARAMETERS
 
-let CURRENT_SEMESTER_END = `1212`  //MMDD
-let CURRENT_SEMESTER_BEGINING_MONDAY = `0826`
+let CURRENT_SEMESTER_END = `0429`  //MMDD
+let CURRENT_SEMESTER_BEGINING_MONDAY = `0112`
 
 //Following optional content helps me know who have used this script-- thank you！
 let YOUR_NAME = `john doe`
